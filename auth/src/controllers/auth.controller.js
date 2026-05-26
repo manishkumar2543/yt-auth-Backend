4d2bde7c82d4392cf55b8c37568a9529b8473645
Manish kumar <memanish2525@gmail.com>
2026-05-26 20:08:07 +0530

    Login  & Logout

--- auth/src/controllers/auth.controller.js
+++ auth/src/controllers/auth.controller.js
@@ -65,13 +65,14 @@ export async function register(req, res) {
             
         
         },
-        accessToken,
-        refreshToken
+        accessToken
+      
     })
 
 }
 export async function getMe(req,res){
     const token = req.headers.authorization?.split(' ')[1];
+    console.log("Token:", token);
     if(!token){
         return res.status(401).json({
             message:'token not provided'
@@ -94,29 +95,184 @@ export async function getMe(req,res){
 } 
 
 export async function refreshToken(req,res){
-    const refreshToken= req.cookies.refreshToken;
+
+    const refreshToken = req.cookies.refreshToken;
+
     if(!refreshToken){
         return res.status(401).json({
             message:'Refresh token not provided'
         })
     }
-    const decoded =jwt.verify(refreshToken, config.JWT_SECRET);
 
-    const accessToken=jwt.sign({
-        id:decoded.id,
+    const decoded = jwt.verify(
+        refreshToken,
+        config.JWT_SECRET
+    );
 
-    },config.JWT_SECRET, {expiresIn:'15m'})
-    const newRefreshToken=jwt.sign({
-        id:decoded.id,
-    },config.JWT_SECRET, {expiresIn:'7d'})
-    res.cookie('refreshToken', newRefreshToken, {
+    const session = await sessionModel.findOne({
+
+        revoked:false
+    });
+
+    if(!session){
+        return res.status(401).json({
+            message:'Invalid refresh token'
+        })
+    }
+
+    const isMatch = await bcrypt.compare(
+        refreshToken,
+        session.refreshTokenHash
+    );
+
+    if(!isMatch){
+        return res.status(401).json({
+            message:'Invalid refresh token'
+        })
+    }
+
+    const accessToken = jwt.sign(
+        { id:decoded.id },
+        config.JWT_SECRET,
+        { expiresIn:'15m' }
+    );
+
+    const newRefreshToken = jwt.sign(
+        { id:decoded.id },
+        config.JWT_SECRET,
+        { expiresIn:'7d' }
+    );
+
+    const newRefreshTokenHash = await bcrypt.hash( newRefreshToken, 10) ;
+
+    session.refreshTokenHash = newRefreshTokenHash;
+
+    await session.save();
+
+    res.cookie('refreshToken', newRefreshToken,{
         httpOnly:true,
         secure:true,
         sameSite:'strict',
         maxAge:7*24*60*60*1000
-    })
+    });
+
     return res.status(200).json({
         message:'Access token refreshed successfully',
+        accessToken
+    });
+}
+
+export async function logout(req,res){
+
+    const refreshToken = req.cookies.refreshToken;
+
+    if(!refreshToken){
+        return res.status(401).json({
+            message:'Refresh token not provided'
+        })
+    }
+
+    const decoded = jwt.verify(
+        refreshToken,
+        config.JWT_SECRET
+    );
+
+    const session = await sessionModel.findOne({
+        revoked:false
+    });
+
+    if(!session){
+        return res.status(401).json({
+            message:'Invalid refresh token'
+        })
+    }
+
+    const isMatch = await bcrypt.compare(
+        refreshToken,
+        session.refreshTokenHash
+    );
+
+    if(!isMatch){
+        return res.status(401).json({
+            message:'Invalid refresh token'
+        })
+    }
+
+    session.revoked = true;
+
+    await session.save();
+
+    res.clearCookie('refreshToken');
+
+    return res.status(200).json({
+        message:'Logged out successfully'
+    })
+}
+
+export async function logoutAll(req,res){
+    const refreshToken = req.cookies.refreshToken;
+    if(!refreshToken){
+        return res.status(401).json({
+            message:'Refresh token not provided'
+        })
+    }
+    const decoded = jwt.verify(
+        refreshToken, 
+        config.JWT_SECRET
+    );
+    await sessionModel.updateMany({
+        user:decoded.id,
+        revoked:false
+    },{
+        revoked:true
+    })
+    res.clearCookie('refreshToken');
+    return res.status(200).json({
+        message:'Logged out from all devices successfully'
+    })
+}
+export async function login(req,res){
+    const { email, password } = req.body;
+    const user= await userModel.findOne({ email });
+    if(!user){
+        return res.status(404).json({
+            message:'User not found'
+        })
+    }
+    const isMatch= await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        return res.status(401).json({
+            message:'Invalid credentials'
+        })
+    }
+    const refreshToken=jwt.sign({
+        id:user._id,
+    }, config.JWT_SECRET, { expiresIn: '7d' });
+    const refreshTokenHash = await bcrypt.hash(refreshToken, 10);
+    const session= await sessionModel.create({
+        user:user._id,
+        refreshTokenHash,
+        ip:req.ip,
+        userAgent:req.headers['user-agent']
+    
+    })
+    const accessToken=jwt.sign({
+        id:user._id,
+        session:session._id
+    }, config.JWT_SECRET, { expiresIn: '15m' });
+
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly:true,
+        secure:true,
+        sameSite:'strict',
+        maxAge:7*24*60*60*1000
+    });
+    return res.status(200).json({
+        message:'Login successful',
+        user:{
+            email:user.email,
+            username:user.username,
+        },
         accessToken
     })
 }
--- auth/src/routes/auth.routes.js
+++ auth/src/routes/auth.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { register, getMe, refreshToken} from "../controllers/auth.controller.js";
+import  { register, getMe, refreshToken,logout, logoutAll, login} from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
 
 authRouter.post('/register', register);
+authRouter.post('/login', login);
 authRouter.get('/get-me', getMe);
 authRouter.get('/refresh-token', refreshToken);
+// logout 
+authRouter.get('/logout', logout);
+authRouter.get('/logout-all', logoutAll);
 
 export default authRouter
